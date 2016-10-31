82019ba2de4c86d77ccdc0f11b8b33808ae67b49
ugofred <ugofred@gmail.com>
2016-10-31 16:00:27 +0000

    edited 2nd assignment

--- lecture2.js
+++ lecture2.js
@@ -1,12 +1,17 @@
 /*
  * 1. explain the special meaning of the following special characters
  \0 the null character
+ \\this is printed when there is nothing in the output
  \n new line
+ This is written when creating line breakers
  \r carriage return
+ Return the cursor to the beginning of the output
  \v vertical tab
- \t
+ \position the form at the next line tab stop
  \backspace
+ move the cursor one position to the left
  \f form feed
+ load the next page
  */
 
 /*
@@ -14,6 +19,7 @@
  *  var arr1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday']
  *  var arr2 = ['Thursday', 'Friday', 'Saturday']
  */
+var arr3 = arr1.concate(arr2);
 
 /*
  *  3.Please combine those three array together as one array by only using concate() function once:
@@ -21,31 +27,42 @@
  *  var arr2 = ['Tuesday', 'Wednesday']
  *  var arr3 = ['Thursday', 'Friday', 'Saturday']
  */
+var arr3 = arr1.concate(arr2, arr3);
 
 /*
  * 4. what is the result(output of following code):
  *    var arr = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
  *    console.log(arr.sort());
+       ["Enoch","Todd","Yaw","ashwin","jerry","lars",mike"]
+       length: 7
+       var arr = []
  *    console.log(arr);
+        length: []
  */
 
 /*
  * 5. what is the result(output of following code):
  *    var arr = [1, 3, 'apple', 'orange', 'black', 'test', 2];
  *    console.log(arr.sort());
+        [1,2,3,'apple','black','orange','test']
+
  *    console.log(arr);
+        [1, 3, 'apple', 'orange', 'black', 'test', 2]
  */
 
 /*
  * 6. Please remove mike, jerry, adn Enoch from the following array by using slice
  *    var arr = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
+            console.log(arr.slice(2,3,5));
  */
 
 /*
  * 7. what is the result(output of following code):
  *    var arr = ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
  *    console.log(arr.slice(-3, -1));
+        ["Yaw","Enoch"]
  *    console.log(arr);
+        ['ashwin', 'Todd', 'mike', 'jerry', 'Yaw', 'Enoch', 'lars'];
  */
 
 
